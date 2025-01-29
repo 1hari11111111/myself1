419ed1056613a27ba0ea5b0ad5ca7df782df0d5f
Hari <uhvardhan987@gmail.com>
2025-01-29 21:13:20 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -79,8 +79,8 @@ async def stats(_, message: Message):
 @new_task
 async def start(client: Client, message: Message):
     buttons = ButtonMaker()
-    buttons.button_link('Owner', f'https://t.me/maheshsirop')
-    buttons.button_link('Group', f'https://t.me/hexafreinds')
+    buttons.button_link('Owner', f'https://t.me/hari_813')
+    buttons.button_link('Group', f'https://t.me/mtpmasala')
     image = config_dict['IMAGE_AUTH']
     user_id = message.from_user.id if message.from_user else message.sender_chat.id
     user_dict = user_data.get(user_id, {})
